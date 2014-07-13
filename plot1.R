ad42ddfdbe38253f763b71e8e8c4d770e9f6737a
xush4 <xush4@sina.com>
2014-07-14 00:41:32 +0800

    Updated on July 12

--- plot1.R
+++ plot1.R
@@ -3,7 +3,9 @@ energyFeb1<-energy[energy$Date=="1/2/2007",]
 energyFeb2<-energy[energy$Date=="2/2/2007",]
 energy2Days<-rbind(energyFeb1,energyFeb2)
 require(stats)
-a=as.numeric(energy2Days$Global_active_power)
+a=as.numeric(energy2Days$Global_active_power,rm.NA=T)
 a=a/500
+png(file = "plot1.png")
 hist(a,col="red",main ="Global Active Power",xlab = "Global Active Power (Kilowatts)")
 dev.copy(png, file = "plot1.png")
+dev.off()
